974c9f17541e0ce9d045b423a2f3014573bd82df
hakobvardanyan <hakvardanyan@gmail.com>
2022-12-03 14:14:01 +0200

    Added third nesting for all menu fragments

--- app/src/main/java/com/hakvardanyan/navigationsample/home/discount/DiscountFragmentChild2.kt
+++ app/src/main/java/com/hakvardanyan/navigationsample/home/discount/DiscountFragmentChild2.kt
@@ -1,12 +1,21 @@
 package com.hakvardanyan.navigationsample.home.discount
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import com.hakvardanyan.navigationsample.BaseFragment
+import com.hakvardanyan.navigationsample.R
 import com.hakvardanyan.navigationsample.databinding.FragmentDiscountChild2Binding
 
 class DiscountFragmentChild2 : BaseFragment<FragmentDiscountChild2Binding>() {
 
     override val bindingInitializer: (LayoutInflater) -> ViewBinding = FragmentDiscountChild2Binding::inflate
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding?.buttonNavigateToDiscountChild3?.setOnClickListener {
+            findNavController().navigate(R.id.action_discountFragmentChild2_to_discountFragmentChild3)
+        }
+    }
 }
--- app/src/main/java/com/hakvardanyan/navigationsample/home/inbox/InboxFragmentChild2.kt
+++ app/src/main/java/com/hakvardanyan/navigationsample/home/inbox/InboxFragmentChild2.kt
@@ -1,12 +1,21 @@
 package com.hakvardanyan.navigationsample.home.inbox
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import com.hakvardanyan.navigationsample.BaseFragment
+import com.hakvardanyan.navigationsample.R
 import com.hakvardanyan.navigationsample.databinding.FragmentInboxChild2Binding
 
 class InboxFragmentChild2 : BaseFragment<FragmentInboxChild2Binding>() {
 
     override val bindingInitializer: (LayoutInflater) -> ViewBinding = FragmentInboxChild2Binding::inflate
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding?.buttonNavigateToInboxChild3?.setOnClickListener {
+            findNavController().navigate(R.id.action_inboxFragmentChild2_to_inboxFragmentChild3)
+        }
+    }
 }
