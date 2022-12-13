51278a4ec97237a5b74e469deb30853b97cdce8a
hakobvardanyan <hakvardanyan@gmail.com>
2022-12-13 20:30:57 +0200

    Added navigation for analysis child fragments

--- app/src/main/java/com/hakvardanyan/navigationsample/main/analysis/AnalysisContainerFragment.kt
+++ app/src/main/java/com/hakvardanyan/navigationsample/main/analysis/AnalysisContainerFragment.kt
@@ -1,12 +1,91 @@
 package com.hakvardanyan.navigationsample.main.analysis
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.viewbinding.ViewBinding
 import com.hakvardanyan.navigationsample.BaseFragment
+import com.hakvardanyan.navigationsample.R
 import com.hakvardanyan.navigationsample.databinding.FragmentAnalysisContainerBinding
+import com.hakvardanyan.navigationsample.main.MainGraphViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AnalysisContainerFragment : BaseFragment<FragmentAnalysisContainerBinding>() {
 
     override val bindingInitializer: (LayoutInflater) -> ViewBinding = FragmentAnalysisContainerBinding::inflate
 
+    private val mainGraphViewModel: MainGraphViewModel by viewModel(ownerProducer = {
+        requireActivity().findNavController(R.id.app_nav_host_container).getBackStackEntry(R.id.mainFragment)
+    })
+
+    private val navController by lazy {
+        requireNotNull(binding).nestedAnalysisNavigationHost.getFragment<NavHostFragment>().navController
+    }
+
+    private val onBackPressedCallback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            navController.popBackStack(navController.graph.findStartDestination().id, false)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, onBackPressedCallback)
+        addDestinationChangeListener()
+        mainGraphViewModel.toolbarBackEvent
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach { navController.popBackStack() }
+            .launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+    private fun addDestinationChangeListener() {
+        navController.addOnDestinationChangedListener(
+            object : NavController.OnDestinationChangedListener {
+                override fun onDestinationChanged(
+                    controller: NavController,
+                    destination: NavDestination,
+                    arguments: Bundle?
+                ) {
+                    if (binding == null) {
+                        controller.removeOnDestinationChangedListener(this)
+                        return
+                    }
+
+                    destination.hierarchy.forEach {
+                        submitToolbarTitle(it.id)
+                        configureBackNavigation(it.id)
+                    }
+                }
+            })
+    }
+
+    private fun submitToolbarTitle(destinationId: Int) = when (destinationId) {
+        R.id.analysisFragment -> R.string.analysis
+        R.id.analysisFragmentChild1 -> R.string.analysis_child_1
+        R.id.analysisFragmentChild2 -> R.string.analysis_child_2
+        R.id.analysisFragmentChild3 -> R.string.analysis_child_3
+        else -> null
+    }?.let(mainGraphViewModel::submitToolbarTitle)
+
+    private fun configureBackNavigation(destinationId: Int) = when (destinationId) {
+        navController.graph.findStartDestination().id -> {
+            onBackPressedCallback.isEnabled = false
+            mainGraphViewModel.showToolbarBackButton(false)
+        }
+        navController.graph.id -> Unit
+        else -> {
+            onBackPressedCallback.isEnabled = true
+            mainGraphViewModel.showToolbarBackButton(true)
+        }
+    }
 }
--- app/src/main/java/com/hakvardanyan/navigationsample/main/analysis/AnalysisFragment.kt
+++ app/src/main/java/com/hakvardanyan/navigationsample/main/analysis/AnalysisFragment.kt
@@ -3,8 +3,10 @@ package com.hakvardanyan.navigationsample.main.analysis
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import com.hakvardanyan.navigationsample.BaseFragment
+import com.hakvardanyan.navigationsample.R
 import com.hakvardanyan.navigationsample.databinding.FragmentAnalysisBinding
 
 class AnalysisFragment : BaseFragment<FragmentAnalysisBinding>() {
@@ -12,9 +14,8 @@ class AnalysisFragment : BaseFragment<FragmentAnalysisBinding>() {
     override val bindingInitializer: (LayoutInflater) -> ViewBinding = FragmentAnalysisBinding::inflate
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding?.apply {
-
+        binding?.pieChart1?.setOnClickListener {
+            findNavController().navigate(R.id.action_analysisFragment_to_analysisFragmentChild1)
         }
     }
-
 }
