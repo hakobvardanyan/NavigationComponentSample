105949159a80e37a0fd169b03e0193da02578233
hakobvardanyan <hakvardanyan@gmail.com>
2022-12-25 19:17:49 +0200

    Created success report screen

--- app/src/main/java/com/hakvardanyan/navigationsample/main/analysis/AnalysisContainerFragment.kt
+++ app/src/main/java/com/hakvardanyan/navigationsample/main/analysis/AnalysisContainerFragment.kt
@@ -72,7 +72,7 @@ class AnalysisContainerFragment : BaseFragment<FragmentAnalysisContainerBinding>
     private fun submitToolbarTitle(destinationId: Int) = when (destinationId) {
         R.id.analysisFragment -> R.string.analysis
         R.id.weeklyAnalysisFragment -> R.string.weekly_analysis
-        R.id.analysisFragmentChild2 -> R.string.analysis_child_2
+        R.id.successReportFragment -> R.string.success
         R.id.analysisFragmentChild3 -> R.string.analysis_child_3
         else -> null
     }?.let(mainGraphViewModel::submitToolbarTitle)
---
+++ app/src/main/java/com/hakvardanyan/navigationsample/main/analysis/SuccessReportFragment.kt
@@ -0,0 +1,26 @@
+package com.hakvardanyan.navigationsample.main.analysis
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBinding
+import com.hakvardanyan.navigationsample.BaseFragment
+import com.hakvardanyan.navigationsample.R
+import com.hakvardanyan.navigationsample.databinding.FragmentSuccessReportBinding
+
+class SuccessReportFragment : BaseFragment<FragmentSuccessReportBinding>() {
+
+    override val bindingInitializer: (LayoutInflater) -> ViewBinding = FragmentSuccessReportBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding?.apply {
+            animationView.postDelayed({
+                animationView.playAnimation()
+            }, 700)
+            buttonClose.setOnClickListener {
+                findNavController().navigate(R.id.action_analysisFragmentChild2_to_analysisFragmentChild3)
+            }
+        }
+    }
+}
